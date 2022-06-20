4052fee32e73166f4fc61b94f3674e1c8f00b32c
Heike Kasper <heike.kasper@kadelo.de>
2022-06-20 14:09:08 +0200

    created boilerplate and plan script

---
+++ script.js
@@ -0,0 +1,8 @@
+// Select how many players: for three 20 Rounds, for four 15 Rounds, for five 12 Rounds and for six 10 Rounds.
+// Input for Names
+// Select if you like to play with advance variation( plus or minus 1) Stiche !== Roundnumber
+// 2 input fields: one for Angesagte Stiche / one for erhaltene Stiche
+// Button who start dealing cards - PopUp how is first (left)
+// count Points (if correct than 20 points + 10 for each Stich / else -10 for each Stich which is different)
+// after Number of Rounds who is the Winner?
+// Reset Game and Reset Round
